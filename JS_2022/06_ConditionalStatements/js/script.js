2623204a56da0aa31fe8fdcd102ab4dc622f6be1
coders-angular <angular.coders@gmail.com>
2022-09-27 11:05:37 +0530

    CS

---
+++ JS_2022/06_ConditionalStatements/js/script.js
@@ -0,0 +1,82 @@
+console.log("Conditional Statements");
+
+/**
+ * Simple If
+ */
+
+
+
+function isEligibility() {
+    var age = document.getElementById('age').value;
+    if (age > 18) { //statement is Executed when condition is true
+        document.getElementById('result').innerHTML = "You are Eligible ,Congratulations";
+    } else {//statement is Executed when condition is false
+        document.getElementById('result').innerHTML = "You are Not Eligible";
+    }
+}
+
+/* Ladder If */
+
+var percent = 60;
+
+if (percent > 35 && percent <= 45) {  //true  && true   ===>true
+    console.log("You are third class")
+}
+if (percent > 45 && percent < 60) {
+    console.log("You are secound class")
+}
+if (percent >= 60 && percent < 74) {
+    console.log("You are first class")
+}
+
+
+/* If else if */
+
+if (percent > 35 && percent <= 45) {  //true  && true   ===>true
+    console.log("You are third class")
+}
+else if (percent > 45 && percent < 60) {
+    console.log("You are secound class")
+}
+else if (percent >= 60 && percent < 74) {
+    console.log("You are first class")
+}
+else {
+    console.log("Not Valid Percentage")
+}
+
+
+function percentage() {
+    var percentage = document.getElementById('p').value;
+
+    if (percentage > 35 && percentage <= 45) {  //true  && true   ===>true
+        document.getElementById('percent').innerHTML = "You are third class";
+        //console.log("You are third class")
+    }
+    else if (percentage > 45 && percentage < 60) {
+        document.getElementById('percent').innerHTML = "You are Secound class";
+        //console.log("You are secound class")
+    }
+    else if (percentage >= 60 && percentage < 74) {
+        document.getElementById('percent').innerHTML = "You are First class";
+        //console.log("You are first class")
+    }
+    else {
+        document.getElementById('percent').innerHTML = "Not Valid Percentage";
+        // console.log("Not Valid Percentage")
+    }
+}
+
+/**
+ * Nested If
+ */
+
+var marks=40;
+
+
+if(marks>50){
+    console.log("Marks Greater than 50")
+    if(marks>60){
+        console.log("Marks Greater than 60")
+    }
+}
